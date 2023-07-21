87037e380bccb04858d501baa3e8db4d50fcb90e
Legis LN <116073320+ama2tourist@users.noreply.github.com>
2023-07-21 13:55:27 +0300

    Initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import typer
+from typing_extensions import Annotated
+
+
+def main(
+    target: Annotated[
+        str, "Pass here file or folder from which image would be extracted"
+    ],
+    destination: Annotated[str, "Pass here where image would be stored"],
+):
+    print("Hello")
+
+
+if __name__ == "__main__":
+    typer.run(main)
